1244ced8ecbb1eb61fe069e3822151ac5337d7a4
xiangze <xiangze@gmail.com>
2012-02-27 02:24:22 +0900

    runtime errors are removed.

--- imBP.R
+++ imBP.R
@@ -8,33 +8,32 @@ imc <- readImage("sample.jpg")
 ima <- imgRGB2Grey(imc)
 
 n <-nrow(ima)
-m <- ncol(ima)
+m <-ncol(ima)
+l <- n*m
 
 img <- expand(ima)
-n1 <- n+1
-m1 <- m+1
+n1 <-nrow(img)
+m1 <-ncol(img)
 
 im <- slideset4(img)
 
 mm <- matrix(0.001,n1,m1)
 zm <- matrix(128.,n1,m1)
 
-m4 <- list(xp=mm,xm=mm,yp=mm,ym=mm)
+m4 <- list(xp=mm,xm=mm,yp=mm,ym=mm,c=mm)
 
-mu <- list(hp=m4,hn=m4,vp=m4,vn=m4)
-lambda <- list(hp=m4,hn=m4,vp=m4,vn=m4)
-
-#muz <- mu
-#lambdaz <- mu
-
-ave <- list(xp=zm,xm=zm,yp=zm,ym=zm)
-var <- list(xp=zm,xm=zm,yp=zm,ym=zm)
+mu <- list(hp=mm,hn=mm,vp=mm,vn=mm)
+lambda <- list(hp=mm,hn=mm,vp=mm,vn=mm)
 
 alpha.hz <- zm
 sig.hz <- zm
 
 dse.em <- 120
 eps <- 100
+
+cat("initdone\n")
+
+cat("alpha.hz,sig.hz loop\n")
 
 for (t in 1:maxem){
   if(dse.em> 0.00001){
@@ -50,8 +49,11 @@ for (t in 1:maxem){
     nr <- 500
     dse <- 10000000
 
+    cat("t=",t,"\n")
+
     for (c in 1:nr){
       if(dse>eps){
+
         nit <- nit+1	
 
         muz <- slideset4.4(mu)
@@ -69,7 +71,7 @@ for (t in 1:maxem){
         r12 <- -ir21/dd
         r21 <- ir12/dd
 
-        cat("ave \n")
+        cat("ave.x \n")
         ave.x1 <- (im$c*isig2+muz$hn$c*lambdaz$hn$c + muz$hp$c*lambdaz$hp$c
                    + muz$vp$c*lambdaz$vp$c+ muz$vn$c*lambdaz$vn$c)/
                      (isig2+lambdaz$hn$c + lambdaz$hp$c +lambdaz$vp$c + lambdaz$vn$c)
@@ -82,10 +84,11 @@ for (t in 1:maxem){
         var.x2 <- r22+ave.x2*ave.x2
         cov.x12 <- r12+ave.x1*ave.x2
         cor.x12 <- var.x1+var.x2-2*cov.x12
-
-        lambda$hp <- 1/(1/alpha+1/(isig2+labmdaz$hp$xm+labmdaz$vp$xm+labmdaz$vm$xm))
-        lambda$hn <- 1/(1/alpha+1/(isig2+labmdaz$hn$xp+labmdaz$vp$xp+labmdaz$vm$xp))
-
+        cat("lambda \n")
+        lambda$hp <- 1/(1/alpha+1/(isig2+lambdaz$hp$xm+lambdaz$vp$xm+lambdaz$vn$xm))
+        lambda$hn <- 1/(1/alpha+1/(isig2+lambdaz$hn$xp+lambdaz$vp$xp+lambdaz$vn$xp))
+        cat("mu \n")
+        
         mu$hp <- (im$xm*isig2+muz$hp$xm*lambdaz$hp$xm+muz$vn$xm*lambdaz$vn$xm
                     +muz$vp$xm*lambdaz$vp$xm)/
                       (isig2+lambdaz$hp$xm+lambdaz$vn$xm+lambdaz$vn$xm)
@@ -94,17 +97,20 @@ for (t in 1:maxem){
                     +muz$vp$xp*lambdaz$vp$xp)/
                       (isig2+lambdaz$hp$xm+lambdaz$vn$xm+lambdaz$vn$xm)
 
-        ir11 <- isig+alpha+lambdaz$hp$c+lambdaz$hn$c+lambdaz$vp$c
-        ir22 <- isig+alpha+lambdaz$hp$yp+lambdaz$hn$yp+lambdaz$vp$yp
+        cat("inv \n")   
+        ir11 <- isig2+alpha+lambdaz$hp$c+lambdaz$hn$c+lambdaz$vp$c
+        ir22 <- isig2+alpha+lambdaz$hp$yp+lambdaz$hn$yp+lambdaz$vp$yp
         ir12 <- -alpha
         ir21 <- -alpha
-#inv
+
         dd <- ir11*ir22-ir12*ir21
         rr11 <- ir22/dd;
         rr22 <- ir11/dd;
         rr12 <- -ir21/dd;
         rr21 <- ir12/dd;
 
+        cat("ave.y \n")
+       
         ave.y1 <- (im$c*isig2+muz$hn$c*lambdaz$hn$c + muz$hp$c*lambdaz$hp$c
                    + muz$vp$c*lambdaz$vp$c+ muz$vn$c*lambdaz$vn$c)/
                      (isig2+lambdaz$hn$c+lambdaz$hp$c+lambdaz$vn$c+lambdaz$vp$c)
@@ -115,49 +121,50 @@ for (t in 1:maxem){
 
         var.y1 <- r11+ave.y1*ave.y1
         var.y2 <- r22+ave.y2*ave.y2
-        cov.y12 <- r12+ave.y1*ave.xy
+        cov.y12 <- r12+ave.y1*ave.y2
         cor.y12 <- var.y1+var.y2-2*cov.y12
 
-        lambda$vp <- 1/(1/alpha+1/(isig2+labmdaz$hp$ym+labmdaz$vp$ym+labmdaz$vm$ym))
-        lambda$vn <- 1/(1/alpha+1/(isig2+labmdaz$hn$yp+labmdaz$vp$yp+labmdaz$vm$yp))
+        lambda$vp <- 1/(1/alpha+1/(isig2+lambdaz$hp$ym+lambdaz$vp$ym+lambdaz$vn$ym))
+        lambda$vn <- 1/(1/alpha+1/(isig2+lambdaz$hn$yp+lambdaz$vp$yp+lambdaz$vn$yp))
 
-        mu.x$vp <- (im.ym*isig2+muz.ym$hp*lambdaz.ym$hp+muz.ym$vn*lambdaz.ym$vn+muz.ym$vp*lambdaz.ym$vp)/(isig2+lambdaz.ym$hp+lambdaz.ym$vn+lambdaz.ym$vn)
+        mu$vp <- (im$ym*isig2+muz$hp$ym*lambdaz$hp$ym+muz$vn$ym*lambdaz$vn$ym
+                    +muz$vp$ym*lambdaz$vp$ym)/
+                      (isig2+lambdaz$hp$ym+lambdaz$vn$ym+lambdaz$vn$ym)
 
-        mu.x$vn <- (im.y*isig2+muz.y$hp*lambdaz.y$hp+muz.y$vn*lambdaz.y$vn+muz.y$vp*lambdaz.y$vp)/(isig2+lambdaz.y$hp+lambdaz.y$vn+lambdaz.y$vn)
+        mu$vn <- (im$yp*isig2+muz$hp$yp*lambdaz$hp$yp+muz$vn$yp*lambdaz$vn$yp
+                  +muz$vp$yp*lambdaz$vp$yp)/
+                    (isig2+lambdaz$hp$yp+lambdaz$vn$yp+lambdaz$vn$yp)
 
-        dse.m <- apply(muz$hp$c-mu$hp$c,c(1,2),abs)
-        +apply(muz$hn$c-mu$hn$c,c(1,2),abs)
-        +apply(muz$vp$c-mu$vp$c,c(1,2),abs)
-        +apply(muz$vn$c-mu$vn$c,c(1,2),abs)
-        +apply(lambdaz$hp$c-lambda$hp$c,c(1,2),abs)
-        +apply(lambdaz$hn$c-lambda$hn$c,c(1,2),abs)
-        +apply(lambdaz$vp$c-lambda$vp$c,c(1,2),abs)
-        +apply(lambdaz$vn$c-lambda$vn$c,c(1,2),abs)
+        dse.m <- apply(muz$hp$c-mu$hp,c(1,2),abs)
+        +apply(muz$hn$c-mu$hn,c(1,2),abs)
+        +apply(muz$vp$c-mu$vp,c(1,2),abs)
+        +apply(muz$vn$c-mu$vn,c(1,2),abs)
+        +apply(lambdaz$hp$c-lambda$hp,c(1,2),abs)
+        +apply(lambdaz$hn$c-lambda$hn,c(1,2),abs)
+        +apply(lambdaz$vp$c-lambda$vp,c(1,2),abs)
+        +apply(lambdaz$vn$c-lambda$vn,c(1,2),abs)
 
         dse <- sum(dse.m)/l
-
 #        muz <- mu
 #        lambdaz <- lambda
-        cor.post.m <- cor.x12+cor.x12
-        cor.post <- sum(cor.post.m)/(l*l)
-
-        zz <- ave.x1+0.5
-
-        fnit <- nit
-        fdse <- dse
+#        fnit <- nit
+#        fdse <- dse
       }#
     }#c
 
-    wk.m <-var.x1-2*im+ave.x1+im*im
+    cor.post.m <- cor.x12+cor.x12
+    cor.post <- sum(cor.post.m)/(l*2)
+
+    wk.m <-var.x1-2*im$c+ave.x1+im$c*im$c
     wk <- sum(wk.m)
 
-    sigmahz <- sqrt(wk/l)
+    sig.hz <- sqrt(wk/l)
     alpha.hz <- 1/(2*cor.post)
-    dse.em <- abs(alpha-alpha.hz)+abs(1/(sig.h*sig.h)-1/(sig.hz*sig.hz))
-    print(c("EM step:",t,"alpha_t=",alpha.hz,"sig_t=",sig.hz))
-
+    dse.em <- mean(abs(alpha-alpha.hz)+abs(1/(sig.h*sig.h)-1/(sig.hz*sig.hz)))
+    cat("EM step:",t,"alpha_t=",alpha.hz,"sig_t=",sig.hz)
+#    print(c("EM step:",t,"alpha_t=",alpha.hz,"sig_t=",sig.hz))
   }
 }
-
-imwrite(zz,"outimage.png")
+zz <- ave.x1+0.5
+writeImage(zz,"outimage.png")
 print(c("alpha_t=",alpha.hz,"sig_t=",sig.hz))
